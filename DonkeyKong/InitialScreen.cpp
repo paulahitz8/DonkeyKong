dc0c618473084b36a8707fb0720a90b29813b5ff
paulahitz8 <paulahitz8@gmail.com>
2020-04-17 18:09:44 +0200

    Initial Screen

--- DonkeyKong/InitialScreen.cpp
+++ DonkeyKong/InitialScreen.cpp
@@ -1,1 +1,62 @@
 #include "InitialScreen.h"
+#include "Application.h"
+#include "ModuleTextures.h"
+#include "ModuleRender.h"
+#include "ModuleAudio.h"
+#include "ModuleInput.h"
+#include "ModuleFadeToBlack.h"
+
+InitialScreen::InitialScreen(bool startEnabled) : Module(startEnabled)
+{
+
+}
+
+InitialScreen::~InitialScreen()
+{
+
+}
+
+// Load assets
+bool InitialScreen::Start()
+{
+	LOG("Loading background assets");
+
+	bool ret = true;
+
+	initialbackground.h = 256;
+	initialbackground.w = 256;
+
+	initialbackgroundtexture = App->textures->Load("Assets/cositasfondo/fondonegro.png");
+	//App->audio->PlayMusic("Assets/Music/introTitle.ogg", 1.0f);
+	App->audio->PlayMusic("Assets/Music/00 - Donkey Kong Main Theme (1).wav", 1.0f);
+
+
+	return ret;
+}
+
+Update_Status InitialScreen::Update()
+{
+	if (App->input->keys[SDL_SCANCODE_SPACE] == Key_State::KEY_DOWN)
+	{
+		App->fade->FadeToBlack(this, (Module*)App->intro, 90);
+	}
+
+	return Update_Status::UPDATE_CONTINUE;
+}
+
+// Update: draw background
+Update_Status InitialScreen::PostUpdate()
+{
+	// Draw everything --------------------------------------
+
+	App->render->Blit(initialbackgroundtexture, 0, 0, &initialbackground, 0);
+
+	return Update_Status::UPDATE_CONTINUE;
+}
+
+bool InitialScreen::CleanUp() {
+
+	App->textures->Unload(initialbackgroundtexture);
+
+	return true;
+}
--- DonkeyKong/InitialScreen.h
+++ DonkeyKong/InitialScreen.h
@@ -1,5 +1,43 @@
 #pragma once
-class InitialScreen
+
+#ifndef __SCENE_INITIAL_H__
+#define __SCENE_INITIAL_H__
+
+#include "Module.h"
+#include "Animation.h"
+
+struct SDL_Texture;
+
+
+class InitialScreen : public Module
 {
+public:
+	//Constructor
+	InitialScreen(bool startEnabled);
+
+	//Destructor
+	~InitialScreen();
+
+	// Called when the module is activated
+	// Loads the necessary textures for the map background
+	bool Start() override;
+
+	// Called at the middle of the application loop
+	// Updates the scene's background animations
+	Update_Status Update() override;
+
+	// Called at the end of the application loop.
+	// Performs the render call of all the parts of the scene's background
+	Update_Status PostUpdate() override;
+
+	bool CleanUp();
+
+public:
+	// The scene sprite sheet loaded into an SDL_Texture
+
+	SDL_Rect initialbackground;
+	SDL_Texture* initialbackgroundtexture = nullptr;
 };
 
+
+#endif
