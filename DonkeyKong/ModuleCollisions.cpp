79c6b0e74149b4e3f6546de0d53add7496f5f783
mav006 <59050192+mav006@users.noreply.github.com>
2020-04-17 17:43:29 +0200

    ModuleCollisions + Colliders

---
+++ DonkeyKong/ModuleCollisions.cpp
@@ -0,0 +1,153 @@
+#include "ModuleCollisions.h"
+
+#include "Application.h"
+
+#include "ModuleRender.h"
+#include "ModuleInput.h"
+#include "SDL/include/SDL_Scancode.h"
+
+ModuleCollisions::ModuleCollisions(bool startEnabled) : Module(startEnabled)
+{
+	for (uint i = 0; i < MAX_COLLIDERS; ++i)
+		colliders[i] = nullptr;
+
+	matrix[Collider::Type::PLAYER][Collider::Type::PLAYER] = false;
+	matrix[Collider::Type::PLAYER][Collider::Type::ENEMY] = true;
+	matrix[Collider::Type::PLAYER][Collider::Type::HAMMER] = false;
+
+	matrix[Collider::Type::ENEMY][Collider::Type::PLAYER] = true;
+	matrix[Collider::Type::ENEMY][Collider::Type::ENEMY] = false;
+	matrix[Collider::Type::ENEMY][Collider::Type::HAMMER] = true;
+
+	matrix[Collider::Type::HAMMER][Collider::Type::PLAYER] = false;
+	matrix[Collider::Type::HAMMER][Collider::Type::ENEMY] = true;
+	matrix[Collider::Type::HAMMER][Collider::Type::HAMMER] = false;
+
+}
+
+// Destructor
+ModuleCollisions::~ModuleCollisions()
+{
+
+}
+
+Update_Status ModuleCollisions::PreUpdate()
+{
+	// Remove all colliders scheduled for deletion
+	for (uint i = 0; i < MAX_COLLIDERS; ++i)
+	{
+		if (colliders[i] != nullptr && colliders[i]->pendingToDelete == true)
+		{
+			delete colliders[i];
+			colliders[i] = nullptr;
+		}
+	}
+
+	Collider* c1;
+	Collider* c2;
+
+	for (uint i = 0; i < MAX_COLLIDERS; ++i)
+	{
+		// skip empty colliders
+		if (colliders[i] == nullptr)
+			continue;
+
+		c1 = colliders[i];
+
+		// avoid checking collisions already checked
+		for (uint k = i + 1; k < MAX_COLLIDERS; ++k)
+		{
+			// skip empty colliders
+			if (colliders[k] == nullptr)
+				continue;
+
+			c2 = colliders[k];
+
+			if (c1->Intersects(c2->rect))
+			{
+				if (matrix[c1->type][c2->type] && c1->listener)
+					c1->listener->OnCollision(c1, c2);
+
+				if (matrix[c2->type][c1->type] && c2->listener)
+					c2->listener->OnCollision(c2, c1);
+			}
+		}
+	}
+
+	return Update_Status::UPDATE_CONTINUE;
+}
+
+Update_Status ModuleCollisions::Update()
+{
+	if (App->input->keys[SDL_SCANCODE_F1] == KEY_DOWN)
+		debug = !debug;
+
+	return Update_Status::UPDATE_CONTINUE;
+}
+
+Update_Status ModuleCollisions::PostUpdate()
+{
+	if (debug)
+		DebugDraw();
+
+	return Update_Status::UPDATE_CONTINUE;
+}
+
+void ModuleCollisions::DebugDraw()
+{
+	Uint8 alpha = 80;
+	for (uint i = 0; i < MAX_COLLIDERS; ++i)
+	{
+		if (colliders[i] == nullptr)
+			continue;
+
+		switch (colliders[i]->type)
+		{
+		case Collider::Type::NONE: // white
+			App->render->DrawQuad(colliders[i]->rect, 255, 255, 255, alpha);
+			break;
+		case Collider::Type::PLAYER: // green
+			App->render->DrawQuad(colliders[i]->rect, 0, 255, 0, alpha);
+			break;
+		case Collider::Type::ENEMY: // red
+			App->render->DrawQuad(colliders[i]->rect, 255, 0, 0, alpha);
+			break;
+		case Collider::Type::HAMMER: // yellow
+			App->render->DrawQuad(colliders[i]->rect, 255, 255, 0, alpha);
+			break;
+		}
+	}
+}
+
+// Called before quitting
+bool ModuleCollisions::CleanUp()
+{
+	LOG("Freeing all colliders");
+
+	for (uint i = 0; i < MAX_COLLIDERS; ++i)
+	{
+		if (colliders[i] != nullptr)
+		{
+			delete colliders[i];
+			colliders[i] = nullptr;
+		}
+	}
+
+	return true;
+}
+
+Collider* ModuleCollisions::AddCollider(SDL_Rect rect, Collider::Type type, Module* listener)
+{
+	Collider* ret = nullptr;
+
+	for (uint i = 0; i < MAX_COLLIDERS; ++i)
+	{
+		if (colliders[i] == nullptr)
+		{
+			ret = colliders[i] = new Collider(rect, type, listener);
+			break;
+		}
+	}
+
+	return ret;
+}
