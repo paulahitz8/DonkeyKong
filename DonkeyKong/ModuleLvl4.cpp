7063080e5382e722f6d21a0c9da0c3e5f00b7cf3
paulahitz8 <paulahitz8@gmail.com>
2020-04-18 22:28:30 +0200

    fsefs

--- DonkeyKong/ModuleLvl4.cpp
+++ DonkeyKong/ModuleLvl4.cpp
@@ -70,6 +70,10 @@ bool ModuleLvl4::Start()
 
 	tile.w = 8;
 	tile.h = 8;
+	back.w = 256;
+	back.h = 256;
+
+	backTexture = App->textures->Load("Assets/cositasfondo/backgroundconcosas.png");
 
 	backgroundTexture = App->textures->Load("Assets/cositasfondo/background.png");
 	floorTexture = App->textures->Load("Assets/cositasfondo/suelo.png");
@@ -208,7 +212,7 @@ Update_Status ModuleLvl4::PostUpdate()
 			}
 		}
 	}
-
+	App->render->Blit(backTexture, 0, 0, &back, 0);
 	return Update_Status::UPDATE_CONTINUE;
 }
 
