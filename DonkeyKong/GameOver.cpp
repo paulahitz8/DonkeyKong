c5505af69a1f1a8a60a919c88330ba3742f117ef
paulahitz8 <paulahitz8@gmail.com>
2020-04-19 22:00:00 +0200

    asas

--- DonkeyKong/GameOver.cpp
+++ DonkeyKong/GameOver.cpp
@@ -8,84 +8,38 @@
 #include "ModulePlayer.h"
 #include "SDL.h"
 
-int map[32][32]{ {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
-					{0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
-					{0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
-					{0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
-					{0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
-					{0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
-					{0,0,0,0,0,0,0,0,0,1,1,1,1,1,1,1,1,1,1,1,1,1,1,0,0,0,0,0,0,0,0,0},
-					{0,0,0,0,0,0,0,0,0,0,7,6,0,0,0,0,0,0,0,0,7,6,0,0,0,0,0,0,0,0,0,0},
-					{0,0,0,0,0,0,0,0,0,0,7,6,0,0,0,0,0,0,0,0,7,6,0,0,0,0,0,0,0,0,0,0},
-					{0,0,0,0,0,0,0,0,0,0,7,6,0,0,0,0,0,0,0,0,7,6,0,0,0,0,0,0,0,0,0,0},
-					{0,0,0,0,0,0,0,0,0,2,7,6,0,0,0,0,0,0,0,0,7,6,2,0,0,0,0,0,0,0,0,0},
-					{0,0,0,0,0,0,5,1,1,3,5,1,1,1,1,1,1,1,1,1,1,5,3,1,1,5,0,0,0,0,0,0},
-					{0,0,0,0,0,0,4,0,0,0,4,0,0,0,0,0,0,0,0,0,0,4,0,0,0,4,0,0,0,0,0,0},
-					{0,0,0,0,0,0,4,0,0,0,4,0,0,0,0,0,0,0,0,0,0,4,0,0,0,4,0,0,0,0,0,0},
-					{0,0,0,0,0,0,4,0,0,0,4,0,0,0,0,0,0,0,0,0,0,4,0,0,0,4,0,0,0,0,0,0},
-					{0,0,0,0,0,0,4,0,0,2,4,0,0,0,0,0,0,0,0,0,0,4,2,0,0,4,0,0,0,0,0,0},
-					{0,0,0,0,0,5,5,1,1,3,5,1,1,1,1,5,1,1,1,1,1,5,3,1,1,5,5,0,0,0,0,0},
-					{0,0,0,0,0,4,0,0,0,0,0,0,0,0,0,4,0,0,0,0,0,0,0,0,0,0,4,0,0,0,0,0},
-					{0,0,0,0,0,4,0,0,0,0,0,0,0,0,0,4,0,0,0,0,0,0,0,0,0,0,4,0,0,0,0,0},
-					{0,0,0,0,0,4,0,0,0,0,0,0,0,0,0,4,0,0,0,0,0,0,0,0,0,0,4,0,0,0,0,0},
-					{0,0,0,0,0,4,0,0,0,2,0,0,0,0,0,4,0,0,0,0,0,0,2,0,0,0,4,0,0,0,0,0},
-					{0,0,0,0,5,5,1,1,1,3,1,5,1,1,1,5,1,1,1,1,5,1,3,1,1,1,5,5,0,0,0,0},
-					{0,0,0,0,4,0,0,0,0,0,0,4,0,0,0,0,0,0,0,0,4,0,0,0,0,0,0,4,0,0,0,0},
-					{0,0,0,0,4,0,0,0,0,0,0,4,0,0,0,0,0,0,0,0,4,0,0,0,0,0,0,4,0,0,0,0},
-					{0,0,0,0,4,0,0,0,0,0,0,4,0,0,0,0,0,0,0,0,4,0,0,0,0,0,0,4,0,0,0,0},
-					{0,0,0,0,4,0,0,0,0,2,0,4,0,0,0,0,0,0,0,0,4,0,2,0,0,0,0,4,0,0,0,0},
-					{0,0,0,5,5,1,1,1,1,3,1,5,1,1,1,1,1,1,1,1,5,1,3,1,1,1,1,5,5,0,0,0},
-					{0,0,0,4,0,0,0,0,0,0,0,0,0,0,0,4,0,0,0,0,0,0,0,0,0,0,0,0,4,0,0,0},
-					{0,0,0,4,0,0,0,0,0,0,0,0,0,0,0,4,0,0,0,0,0,0,0,0,0,0,0,0,4,0,0,0},
-					{0,0,0,4,0,0,0,0,0,0,0,0,0,0,0,4,0,0,0,0,0,0,0,0,0,0,0,0,4,0,0,0},
-					{0,0,0,4,0,0,0,0,0,0,0,0,0,0,0,4,0,0,0,0,0,0,0,0,0,0,0,0,4,0,0,0},
-					{0,0,1,5,1,1,1,1,1,1,1,1,1,1,1,5,1,1,1,1,1,1,1,1,1,1,1,1,5,1,0,0},
-};
 
 GameOver::GameOver(bool startEnabled) : Module(startEnabled)
 {
 }
 
-
 GameOver::~GameOver()
 {
 }
-
 
 bool GameOver::Start()
 {
 	LOG("Loading background assets");
 
-	//TILEMAP
+	bool ret = true;
 
-	tile.w = 8;
-	tile.h = 8;
+	backgroundgameover.h = 256;
+	backgroundgameover.w = 256;
 
-
-	//backgroundTexture = App->textures->Load("Assets/cositasfondo/background.png");
-	floorTexture = App->textures->Load("Assets/cositasfondo/suelo.png");
-	floorCarrotTexture = App->textures->Load("Assets/cositasfondo/sueloencimazanahoria.png");
-	carrotTexture = App->textures->Load("Assets/cositasfondo/zanahoria.png");
-	ladderTexture = App->textures->Load("Assets/cositasfondo/escalera.png");
-	floorLadderTexture = App->textures->Load("Assets/cositasfondo/sueloescalera.png");
-	leftPoleTexture = App->textures->Load("Assets/cositasfondo/barraizquierda.png");
-	rightPoleTexture = App->textures->Load("Assets/cositasfondo/barraderecha.png");
+	backgroundgameoverTexture = App->textures->Load("Assets/cositasfondo/gameover.png");
 	
+	App->audio->PlayMusic("Assets/Music/silence.wav", 1.0f);
 
-	return true;
+
+	return ret;
 }
-
 
 Update_Status GameOver::Update()
 {
 	if (App->input->keys[SDL_SCANCODE_SPACE] == Key_State::KEY_DOWN)
 	{
-		App->player->livecount = 3;
 		App->fade->FadeToBlack(this, (Module*)App->lvl4, 30);
 	}
-
-	// en teoria se acaba el juego aunque podriamos poner un try again o algo
-	// si pulsa una tecla
 
 	return Update_Status::UPDATE_CONTINUE;
 }
@@ -93,55 +47,14 @@ Update_Status GameOver::Update()
 
 Update_Status GameOver::PostUpdate()
 {
-	int b = 0;
+	App->render->Blit(backgroundgameoverTexture, 0, 0, &backgroundgameover, 0);
 
-	for (int column = 0; column < 32; column++) {
-		for (int row = 0; row < 32; row++) {
-
-			b = map[column][row];
-
-			switch (b) {
-
-		/*	case 0:
-				App->render->Blit(backgroundTexture, row * 8, column * 8, &tile, 0);
-				break;*/
-
-			case 1:
-				App->render->Blit(floorTexture, row * 8, column * 8, &tile, 0);
-				break;
-
-			case 2:
-				App->render->Blit(floorCarrotTexture, row * 8, column * 8, &tile, 0);
-				break;
-
-			case 3:
-				App->render->Blit(carrotTexture, row * 8, column * 8, &tile, 0);
-				break;
-
-			case 4:
-				App->render->Blit(ladderTexture, row * 8, column * 8, &tile, 0);
-				break;
-
-			case 5:
-				App->render->Blit(floorLadderTexture, row * 8, column * 8, &tile, 0);
-				break;
-
-			case 6:
-				App->render->Blit(leftPoleTexture, row * 8, column * 8, &tile, 0);
-				break;
-
-			case 7:
-				App->render->Blit(rightPoleTexture, row * 8, column * 8, &tile, 0);
-				break;
-			}
-		}
-	}
-	
 	return Update_Status::UPDATE_CONTINUE;
 }
 
-
 bool GameOver::CleanUp() {
+
+	App->textures->Unload(backgroundgameoverTexture);
 
 	return true;
 }
--- DonkeyKong/GameOver.h
+++ DonkeyKong/GameOver.h
@@ -12,27 +12,12 @@ struct SDL_Texture;
 class GameOver : public Module
 {
 
-private:
+public:
 
-	//TILEMAP
-	SDL_Rect back;
-	SDL_Rect tile;
 
-	SDL_Texture* backTexture = nullptr;
-	SDL_Texture* backgroundTexture = nullptr;
-	SDL_Texture* floorTexture = nullptr;
-	SDL_Texture* floorCarrotTexture = nullptr;
-	SDL_Texture* carrotTexture = nullptr;
-	SDL_Texture* ladderTexture = nullptr;
-	SDL_Texture* floorLadderTexture = nullptr;
-	SDL_Texture* leftPoleTexture = nullptr;
-	SDL_Texture* rightPoleTexture = nullptr;
-	SDL_Texture* liveTexture = nullptr;
+	SDL_Rect backgroundgameover;
+	SDL_Texture* backgroundgameoverTexture = nullptr;
 
-	int i = 1;
-	int j = 0;
-
-	int map[32][32];
 
 
 public:
