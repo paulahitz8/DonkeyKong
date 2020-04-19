b2e6a967315bac39444755a8005b8f1c762aed1c
paulahitz8 <paulahitz8@gmail.com>
2020-04-19 15:50:36 +0200

    RGDFTG

--- DonkeyKong/GameOver.cpp
+++ DonkeyKong/GameOver.cpp
@@ -5,37 +5,36 @@
 #include "ModuleAudio.h"
 #include "ModuleInput.h"
 #include "ModuleFadeToBlack.h"
+#include "ModulePlayer.h"
 
 GameOver::GameOver(bool startEnabled) : Module(startEnabled)
 {
 }
 
+
 GameOver::~GameOver()
 {
 }
 
-// Load assets
+
 bool GameOver::Start()
 {
 	LOG("Loading background assets");
 
 	bool ret = true;
-
 	background.h = 256;
 	background.w = 256;
-
 	backgroundTexture = App->textures->Load("Assets/cositasfondo/fondonegro.png");
-	//App->audio->PlayMusic("Assets/Music/introTitle.ogg", 1.0f);
-	//App->audio->PlayMusic("Assets/Music/00 - Donkey Kong Main Theme (1).wav", 1.0f);
-
-
+	
 	return ret;
 }
+
 
 Update_Status GameOver::Update()
 {
 	if (App->input->keys[SDL_SCANCODE_SPACE] == Key_State::KEY_DOWN)
 	{
+		App->player->livecount = 3;
 		App->fade->FadeToBlack(this, (Module*)App->lvl4, 30);
 	}
 
@@ -44,15 +43,14 @@ Update_Status GameOver::Update()
 	return Update_Status::UPDATE_CONTINUE;
 }
 
-// Update: draw background
+
 Update_Status GameOver::PostUpdate()
 {
-	// Draw everything --------------------------------------
-
 	App->render->Blit(backgroundTexture, 0, 0, &background, 0);
 
 	return Update_Status::UPDATE_CONTINUE;
 }
+
 
 bool GameOver::CleanUp() {
 
--- DonkeyKong/PierdeVida.h
+++ DonkeyKong/PierdeVida.h
@@ -11,29 +11,23 @@ struct SDL_Texture;
 class PierdeVida : public Module
 {
 
-	int i = 1;
+	int j = 1;
+
+	SDL_Rect back;
+	SDL_Texture* backTexture = nullptr;
+
+	SDL_Rect tile;
+	SDL_Texture* liveTexture = nullptr;
+	SDL_Texture* backgroundTexture = nullptr;
+	
 
 public:
-	//Constructor
 	PierdeVida(bool startEnabled);
-	//Destructor
 	~PierdeVida();
-	// Called when the module is activated
-	// Loads the necessary textures for the map background
 	bool Start() override;
-	// Called at the middle of the application loop
-	// Updates the scene's background animations
 	Update_Status Update() override;
-	// Called at the end of the application loop.
-	// Performs the render call of all the parts of the scene's background
 	Update_Status PostUpdate() override;
 	bool CleanUp();
-
-public:
-	// The scene sprite sheet loaded into an SDL_Texture
-
-	SDL_Rect background;
-	SDL_Texture* backgroundTexture = nullptr;
 
 };
 
