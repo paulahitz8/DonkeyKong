b3ff28d0314159610c2d1933361a9f5ec519fd4c
ihedud <irene.hepo@gmail.com>
2020-06-14 10:56:43 +0200

    ya he acabado el movimiento de los enemigos

--- DonkeyKong/EnemyCakes.cpp
+++ DonkeyKong/EnemyCakes.cpp
@@ -14,32 +14,22 @@ Enemy_Cakes::Enemy_Cakes(int x, int y) : Enemies(x, y)
 void Enemy_Cakes::Update()
 {
 	position.x += speed;
-	/*if (position.y == 120)
-	{*/
-		if (position.x < 24) {
-			speed = -speed;
-		}
-		if (position.x > 220) {
-			speed = -speed;
-		}
-	/*}*/
-	/*if (position.y == 156)
+	if (position.x < 24) {
+		speed = -speed;
+	}
+	if (position.x > 220) {
+		speed = -speed;
+	}
+	if (position.y == 120)
 	{
-		if (position.x < 32) {
-			currentAnim = &rightAnim;
+		if (position.x == 144)
+		{
+			speed = -speed;
 		}
-		if (position.x > 212) {
-			currentAnim = &leftAnim;
+		if (position.x == 96)
+		{
+			speed = -speed;
 		}
 	}
-	if (position.y == 116)
-	{
-		if (position.x < 40) {
-			currentAnim = &rightAnim;
-		}
-		if (position.x > 206) {
-			currentAnim = &leftAnim;
-		}
-	}*/
 	Enemies::Update();
 }
